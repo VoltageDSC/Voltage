f23c15505d597ec37b391e5dade47ff7caea0123
Slddev <sldemonatnoiv@gmail.com>
2023-03-07 13:45:53 +0800

    Bump to 1.1.0

--- src/voltage/plugins/CrashHandler.ts
+++ src/voltage/plugins/CrashHandler.ts
@@ -86,7 +86,7 @@ export default definePlugin({
         setTimeout(() => crashCount--, 60_000);
 
         try {
-            if (crashCount === 1) maybePromptToUpdate("Uh oh, Discord has just crashed... but good news, there is a Vencord update available that might fix this issue! Would you like to update now?", true);
+            if (crashCount === 1) maybePromptToUpdate("Uh oh, Discord has just crashed... but good news, there is a Voltage update available that might fix this issue! Would you like to update now?", true);
 
             if (settings.store.attemptToPreventCrashes) {
                 this.handlePreventCrash(_this);
